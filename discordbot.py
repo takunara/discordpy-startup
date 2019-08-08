271635106b2855fc2342072140da4e3d12a473c2
takunara <53904196+takunara@users.noreply.github.com>
2019-08-08 22:46:42 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,12 +13,25 @@ token = os.environ['DISCORD_BOT_TOKEN']
 bot = discord.Client() # 接続に使用するオブジェクト
 
 @bot.event
+
+async def on_ready():
+    while True:
+        channel = client.get_channel('20分模写')
+        await bot.send_message(channel, 'おはよう')
+        time.sleep(10)
+
+client.run(token)
+
+
+"""
 async def on_ready():
     while True:
         if time.strftime('%H:%M',time.localtime())=='22:33':
             channel = client.get_channel('20分模写')
             await client.send_message(channel, '勝手に喋るよ')
             sleep(10)
+            
+"""            
 #------------------------------    
     
 #@bot.event
