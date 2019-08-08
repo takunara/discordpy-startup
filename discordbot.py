a86a7091a3aaa682b9d91f31e1bdfe0efbae78ca
takunara <53904196+takunara@users.noreply.github.com>
2019-08-08 22:49:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,41 +5,14 @@ import time
 from time import sleep
 import threading
 
-#bot = commands.Bot(command_prefix='/')
+bot = discord.Client() # 接続に使用するオブジェクト
 token = os.environ['DISCORD_BOT_TOKEN']
 
-
-#追加--------------------------
-bot = discord.Client() # 接続に使用するオブジェクト
-
 @bot.event
-
 async def on_ready():
     while True:
         channel = client.get_channel('20分模写')
         await bot.send_message(channel, 'おはよう')
         time.sleep(10)
-
-client.run(token)
-
-
-"""
-async def on_ready():
-    while True:
-        if time.strftime('%H:%M',time.localtime())=='22:33':
-            channel = client.get_channel('20分模写')
-            await client.send_message(channel, '勝手に喋るよ')
-            sleep(10)
-            
-"""            
-#------------------------------    
-    
-#@bot.event
-#async def on_command_error(ctx, error):
-#    await ctx.send(str(error))
-#@bot.command()
-#async def ping(ctx):
-#    await ctx.send('pong')
-
 
 bot.run(token)
