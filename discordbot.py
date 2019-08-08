c02d48247c04e9f7edb8c4d2eccdabdb99108c43
takunara <53904196+takunara@users.noreply.github.com>
2019-08-09 00:09:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,13 +15,4 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
-# 60秒に一回ループ
-@tasks.loop(seconds=60)
-async def loop():
-    channel = client.get_channel("20分模写")
-    await channel.send('時間だよ')  
-
-#ループ処理実行
-loop.start()
-
 bot.run(token)
