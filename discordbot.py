a737aacfe97e17b1bb787c6d80a028bb0a74b7b1
takunara <53904196+takunara@users.noreply.github.com>
2019-08-09 07:18:19 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,7 +13,9 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong--')
+#    await ctx.send('pong--')
+    channel_bot_test = [channel for channel in client.get_all_channels() if channel.name == '20分模写'][0]
+    await ctx.send_message(channel_bot_test, '勝手に喋るよ')
 
 
 bot.run(token)
