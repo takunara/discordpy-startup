fa49ff9638903a29c7f06608cda127b0efab9af2
takunara <53904196+takunara@users.noreply.github.com>
2019-08-09 07:40:35 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -34,7 +34,7 @@ async def on_ready():
     asyncio.ensure_future(greeting_gm())
 
 async def greeting_gm():
-    await client.send_message(channel, 'おはよう')
+    await client.send_message("test", 'おはよう')
     await asyncio.sleep(10)
 
 client.run(token)
