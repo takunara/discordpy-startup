1299b8cc8e359e84a2e0e724efbc0604b5961081
takunara <53904196+takunara@users.noreply.github.com>
2019-08-09 07:57:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,4 +1,4 @@
-"""
+
 from discord.ext import commands
 import os
 import traceback
@@ -18,32 +18,3 @@ async def ping(ctx):
 
 
 bot.run(token)
-"""
-# インストールした discord.py を読み込む
-import discord
-import os
-
-# 自分のBotのアクセストークンに置き換えてください
-TOKEN = os.environ['DISCORD_BOT_TOKEN']
-
-# 接続に必要なオブジェクトを生成
-client = discord.Client()
-
-# 起動時に動作する処理
-@client.event
-async def on_ready():
-    # 起動したらターミナルにログイン通知が表示される
-    print('ログインしました')
-
-# メッセージ受信時に動作する処理
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
-
-# Botの起動とDiscordサーバーへの接続
-client.run(TOKEN)
