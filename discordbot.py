8fd4a21811d723ae0c713c428a722d0e0db29588
takunara <53904196+takunara@users.noreply.github.com>
2019-08-09 08:24:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,11 +1,11 @@
-"""
+
 from discord.ext import commands
 import os
 import traceback
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-
+CHANNEL_ID = 608981862735675423
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -18,23 +18,3 @@ async def ping(ctx):
 
 
 bot.run(token)
-"""
-
-import os
-import discord
-from discord.ext import tasks
-
-TOKEN = os.environ['DISCORD_BOT_TOKEN']
-CHANNEL_ID = 608981862735675423
-client = discord.Client()
-
-# 60秒に一回ループ
-@tasks.loop(seconds=60)
-async def loop():
-    channel = client.get_channel(CHANNEL_ID)
-    await channel.send('時間だよ')  
-
-#ループ処理実行
-loop.start()
-# Botの起動とDiscordサーバーへの接続
-client.run(TOKEN)
