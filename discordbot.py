c64a95f2114f78d682e3b1d94616bda2e31be258
takunara <53904196+takunara@users.noreply.github.com>
2019-08-08 22:24:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,19 +1,30 @@
 from discord.ext import commands
 import os
 import traceback
+import time
+from time import sleep
+import threading
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
+#追加--------------------------
 @bot.event
-async def on_command_error(ctx, error):
-    await ctx.send(str(error))
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def on_ready():
+    while True:
+        if time.strftime('%H:%M',time.localtime())=='22:26':
+            channel = client.get_channel('20分模写')
+            await client.send_message(channel, '勝手に喋るよ')
+            sleep(60)
+#------------------------------    
+    
+#@bot.event
+#async def on_command_error(ctx, error):
+#    await ctx.send(str(error))
+#@bot.command()
+#async def ping(ctx):
+#    await ctx.send('pong')
 
 
 bot.run(token)
